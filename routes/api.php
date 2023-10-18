4dc0380b511c9a6679f3bdf4685185cb09836a6c
adlgkjs <99225585+adlgkjs@users.noreply.github.com>
2023-10-17 19:07:09 -0600

    Add files via upload

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,8 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\MovieController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+//Con esta sola ruta ya tenemos todos los metodos necesario para consultar y añadir registros a la base de datos
+// verificarlo con  el comando: php artisan route:list
+Route::apiResource('movies', MovieController::class);
